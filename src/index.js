41e241620a02e53e6836f286695d8535ecf3c395
Sherita Sherlene <sheritsh@va-f3.kzn.21-school.ru>
2023-01-09 00:14:41 +0300

    deploy: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) n *= -1;
+  let strNumb = n.toString();
+  res = '';
+  for(let i = 0; i < strNumb.length; i++) {
+    res = `${res}${strNumb[strNumb.length - 1 - i]}`
+  }
+  return res;
 }
